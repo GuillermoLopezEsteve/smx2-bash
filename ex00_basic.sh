ad84e520abf5249dbe57c897f121983ee5d956d5
GuillermoLopezEsteve <guillermolopez.2000@gmail.com>
2026-04-07 15:44:30 +0200

    Create ex00_basic.sh

---
+++ ex00_basic.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# ==========================================
+# EXERCICI 0 -  basics
+# ==========================================
+#
+# L'script ha de:
+#   llegir una path a un fitxer i veure si existeix, en el cas que no existeixi crear-lo
+#   llegir un usuari, en el cas que no existeixi crea l'usuari amb la contrasenya exemple
